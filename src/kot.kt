fe15b4e9e51a3d07226f741c872b48b4c5824c44
ampuriraelizabeth <elizabethampurira@gmail.com>
2022-02-23 18:52:24 +0300

    first commit

--- src/kot.kt
+++ src/kot.kt
@@ -1,11 +1,13 @@
 fun main(){
     var note = "hello"
     var name = "liz"
-    var add = "hello"+"liz"
-    println("hello "+" "+"liz")
+    var sum = "hello"+"liz"
+    println("hello"+ " "+"liz")
     one()
     two()
-
+three()
+    four()
+    five()
 }
 fun sum(hello:String,liz:String):String{
     var sum =("fact"+"liz")
@@ -17,7 +19,7 @@ fun sum(hello:String,liz:String):String{
 
 
 fun one(){
-    var x=remainder(90,11)
+   var x= remainder(90,19)
     println(x)
 
 }
@@ -34,4 +36,34 @@ fun two(){
 fun l(x:Int,y:Int):Int{
     var modulus =x%y
             return modulus
+}
+
+fun three (){
+    var u=add(30, 40)
+    println(u)
+}
+fun add(y:Int,h:Int):Int{
+    var sum=y+h
+    return sum
+
+}
+
+fun four(){
+  var name=" liz"
+  var tribe="bakiiga"
+    var both= "liz "+" is a" +"bakiiga"
+    println(both)
+}
+fun tribe(liz:String,bakiiga:String ):String{
+     var tribe=liz+bakiiga
+     return tribe
+}
+fun five(){
+ var peep= product(400,30)
+  var product= 400*30
+    println(peep)
+}
+fun product(m:Int,n:Int):Int{
+    var product=m*n
+    return product
 }
